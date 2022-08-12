c12c9927254fe478eb5b839a47d8a64124966b85
Enzuo <potc.zone+git@gmail.com>
2022-08-12 15:16:55 +0200

    Add data

--- data-english.js
+++ data-english.js
@@ -2,8 +2,21 @@ const DATA = [
     // NAME                  , namecode              , platforms          , type     ,"story"
     ["No Protocol"           , "noprotocol"          , [["youtube", 19.2]], "comment",],
     ["The Authentic Observer", "theauthenticobserver", [["youtube", 57.6]], ""       ,],
+    ["Kidology"              , "kidology"            , [["youtube", 97.8]], ""       ,],
+    ["Kat Blaque"            , "katblaque"           , [["youtube", 458 ]], ""       ,],
+    ["Xhanderhal"            , "xanderhal"           , [["youtube", 71.6]], ""       ,],
+    ["Vaush"                 , "vaush"               , [["youtube", 406]] , ""       ,],
+    ["Triggernometry"        , "triggernometry"      , [["youtube", 337]] , ""       ,],
+    ["Just Pearly"           , "justpearlythings"    , [["youtube", 249]] , ""       ,],
 ]
 
 const DATAXY = [
-    [50,50]
+    [622, 437],
+    [492, 383],
+    [518, 297],
+    [210, 183],
+    [124, 357],
+    [268, 232],
+    [735, 201],
+    [802, 251]
 ]
